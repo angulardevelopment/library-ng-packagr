a6a8bc019050360c7af07aea5718b4f918e0956e
Gupta, Sunny <sunnyvales789@gmail.com>
2024-03-09 15:30:28 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormatCodeModule } from 'projects/format-code/src/public-api';
-import { ToastModule } from 'projects/toast/src/public-api';
+import { ToastModule } from 'toast';
+// import { ToastModule } from 'projects/toast/src/public-api';
 
 @NgModule({
   declarations: [
